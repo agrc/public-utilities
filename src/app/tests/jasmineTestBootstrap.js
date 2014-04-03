4edb2570eef4c1d084a9780091a150e1689a0c11
steveoh <sgourley@utah.gov>
2014-04-03 17:15:36 -0600

    try to get travis to run all tests

--- src/app/tests/jasmineTestBootstrap.js
+++ src/app/tests/jasmineTestBootstrap.js
@@ -3,5 +3,5 @@ var dojoConfig = {
     has: {'dojo-undef-api': true},
     // packages: ['matchers'],
     isJasmineTestRunner: true,
-    packages: ['matchers']
+    packages: ['matchers', 'stubmodule']
 };
